0446118729209b5c09c1ec0afd78e07dab1ac0b2
anson <anson@ricequant.com>
2020-01-08 15:26:43 +0800

    ci: add package verison env

--- scripts/loadenv.sh
+++ scripts/loadenv.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+sed -i '' '/VERSION/d' .env
+echo VERSION=$(cat package.json|grep '"version":'|cut -d'"' -f4) >> .env
+
 ENV=$(grep -v '^#' .env | xargs)
 export $ENV
