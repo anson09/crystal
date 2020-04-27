69a91145af29d147243d4821fc6bd7ed364cafae
anson <anson@ricequant.com>
2020-04-27 19:00:04 +0800

    fix: linux sed fail

--- scripts/loadenv.sh
+++ scripts/loadenv.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-sed -i '' '/VERSION/d' .env
+echo "$(sed '/VERSION/d' .env)" > .env
 echo VERSION=$(cat package.json|grep '"version":'|cut -d'"' -f4) >> .env
 
 ENV=$(grep -v '^#' .env | xargs)
