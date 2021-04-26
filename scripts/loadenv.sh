2ca221a0faac3c64560abe205667bef697c64b90
anson <anson@ricequant.com>
2021-04-26 11:52:37 +0800

    chore: updep

--- scripts/loadenv.sh
+++ scripts/loadenv.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-echo "$(sed '/VERSION/d' .env)" > .env
-echo VERSION=$(cat package.json|grep '"version":'|cut -d'"' -f4) >> .env
+echo "$(sed '/VERSION/d' .env)" >.env
+echo VERSION=$(cat package.json | grep '"version":' | cut -d'"' -f4) >>.env
 
 ENV=$(grep -v '^#' .env | xargs)
 REQUIRES=('PUBLIC_PATH')
 
-for ITEM in ${REQUIRES[@]}
-    do
-        if [[ ! $ENV =~ $ITEM ]]
-        then
-            echo "script fail: .env missing item - < ${ITEM} >\n"
-            exit 1
-        fi
-    done
+for ITEM in ${REQUIRES[@]}; do
+    if [[ ! $ENV =~ $ITEM ]]; then
+        echo "script fail: .env missing item - < ${ITEM} >\n"
+        exit 1
+    fi
+done
 
 export $ENV
